2b748456c63b70f6ee8121b6a7a04c4ba7d3f71f
HernanRRD <43017231+hernanrrd110@users.noreply.github.com>
2021-12-14 20:54:24 -0300

    Update Remocion_artefactos_Lupa.m

--- Pipeline/Remocion_artefactos_Lupa.m
+++ Pipeline/Remocion_artefactos_Lupa.m
@@ -14,40 +14,96 @@ addpath('./Imagenes');
 [M,N,t] = size(imRGB);
 
 %% ======= Deteccion de LUPA
+
 [imCort, posCent, radio] = detectorlupa(imRGB);
-f = figure('Name', 'imagen orignal y LUPA');
-imshowpair(imRGB,imCort,'montage'); title('imagen orignal y LUPA');
+radio = round(radio);
+posCent = round(posCent);
+figure();
+imshow(imCort);
+%%
+tolRadio = 20;
+posY1 = posCent(2) - radio - tolRadio;
+posY2 = posCent(2) + radio + tolRadio;
+posX1 = posCent(1) - radio - tolRadio;
+posX2 = posCent(1) + radio + tolRadio;
+
+while(0>posY1)
+    tolRadio = tolRadio - 1;
+    posY1 = posCent(2) - radio - tolRadio;
+end
+
+tolRadio = 20;
+while(posY2>M)
+    tolRadio = tolRadio - 1;
+    posY2 = posCent(2) + radio + tolRadio;
+end
+
+tolRadio = 20;
+while(0>posX1)
+    tolRadio = tolRadio - 1;
+    posX1 = posCent(1) - radio - tolRadio;
+end
+
+tolRadio = 20;
+while(posX2>N)
+    tolRadio = tolRadio - 1;
+    posX2 = posCent(1) + radio + tolRadio;
+end
+
+% 
+imCort2 = imRGB(posY1:posY2,posX1:posX2,:);
+
+figure();
+imshow(imCort2);
 
 %%
 close all;
 % -- Filtrado de mediana a la imagen
-ventAlto = 50; ventAncho = 50;
-imMediana(:,:,1) = medfilt2(imRGB(:,:,1) ,[ventAlto ventAncho]);
-imMediana(:,:,2) = medfilt2(imRGB(:,:,2) ,[ventAlto ventAncho]);
-imMediana(:,:,3) = medfilt2(imRGB(:,:,3) ,[ventAlto ventAncho]);
+ventAlto = 15; ventAncho = 15;
+
+imRGBGPU = gpuArray(imRGB);
+imMediana = imRGBGPU;
+
+iter = 3;
+for i = 1:iter
+    if (iter==1)
+        imMediana(:,:,1) = medfilt2(imRGBGPU(:,:,1) ,...
+            [ventAlto ventAncho]);
+        imMediana(:,:,2) = medfilt2(imRGBGPU(:,:,2) ,...
+            [ventAlto ventAncho]);
+        imMediana(:,:,3) = medfilt2(imRGBGPU(:,:,3) ,...
+            [ventAlto ventAncho]);
+    else
+        imMediana(:,:,1) = medfilt2(imMediana(:,:,1) ,...
+            [ventAlto ventAncho]);
+        imMediana(:,:,2) = medfilt2(imMediana(:,:,2) ,...
+            [ventAlto ventAncho]);
+        imMediana(:,:,3) = medfilt2(imMediana(:,:,3) ,...
+            [ventAlto ventAncho]);
+    end
+end
+imMediana = gather(imMediana);
+f = figure('Name', 'Mediana');
+imshow(imMediana);
 
 %%
 % ======== Graficacion imagen original y filtrada
-f = figure('Name', 'imagen original y con filtro de mediana');
-imshowpair(imRGB,imMediana,'montage')
-% set(f,'WindowStyle','docked')
 
 % Operacion de contraste de Weber
-contrast_Weber = (imRGB(:,:,2) - imMediana(:,:,2))...
+contrastWeber = (imRGB(:,:,2) - imMediana(:,:,2))...
     ./imMediana(:,:,2); 
 
-tc = 0.01; % valor de tolerancia
-im_modif = imRGB;
+tc = 0.2; % valor de tolerancia
+imModif = imRGB;
 
 for i=1:M
     for j=1:N
-        if(contrast_Weber(i,j)>tc)
-            im_modif(i,j,:) = imMediana(i,j,:);
+        if(contrastWeber(i,j)>tc)
+            imModif(i,j,:) = imMediana(i,j,:);
         end
     end
 end
 
 f = figure('Name', 'imagen original y modificada');
-imshowpair(imRGB,im_modif,'montage')
-set(f,'WindowStyle','docked')
+imshowpair(imRGB,imModif,'montage')
 
